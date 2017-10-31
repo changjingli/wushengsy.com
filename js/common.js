6c6ce70fce81cecde17993b465fe1ee07cd2e5f4
changjingli <jingli_chang@foxmail.com>
2017-11-01 01:13:09 +0800

    统一整改后代码

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function () {
+	$(".btn-menu").on("touchstart", function () {
+		/*
+		 * .show common.css   line 236
+		 * .oh   function.css line 7
+		 */
+		$(".ws_nav").toggle("show");
+	});
+});
