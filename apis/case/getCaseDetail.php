fc6db13854fcfa424ecb70636908af8544ce6a41
Chang, Jingli <jingli_chang@foxmail.com>
2018-06-17 17:43:10 +0800

    后台案例详情接口

--- apis/case/getCaseDetail.php
+++ apis/case/getCaseDetail.php
@@ -1,46 +1,14 @@
 <?php
-	require_once "../conn.php";
-	/*
-	 * @desc 根据制定ID获取案例详情
-	 * @author Chang, Jingli
-	 */
-	
-	header('Content-type: application/json;charset=utf-8');
-	
-	$id = @$_REQUEST[ 'id' ];
-	
-	if (!$id) {
-		die( json_encode(array(
-			"code" => "2000",
-			"desc" => "没有id",
-		), JSON_UNESCAPED_UNICODE) );
-	}
-	// sql 查询
-	$query = "select * from wusheng.case where id=".$id;
-	mysqli_query($link,"set character set 'utf8'");//读库
-	// 执行sql查询
-	$result = mysqli_query($link, $query) or die("sql exec failed");
-	
-	$arr = [];
-	
-	while ( $row = mysqli_fetch_array($result) ) {
-		$arr = array(
-			"id"      => $row['id'     ], // 案例id
-			"title"   => $row['title'  ], // 案例标题
-			"type"    => $row['type'   ], // 案例标题
-			"author"  => $row['author' ], // 作者
-			"time"    => $row['time'   ], // 发布时间
-			"content" => $row['content'], // 案例内容
-			"view"    => $row['view'   ], // 浏览次数
-		);
-	}
-	
-	// 输出查询结果
-	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
-	
-	// 对应案例访问量增加1
-	mysqli_query( $link, "UPDATE wusheng.case SET view=view+1 where id=".$id );
-		
-	// 关闭数据库连接
-	mysqli_close($link);
-?>
+/**
+ * Created by PhpStorm.
+ * User: jingl
+ * Date: 2018/6/15
+ * Time: 18:04
+ * Desc: 获取案例详情
+ */
+require_once '../common/ws_case.php';
+
+header( 'Content-type: application/json;charset=utf-8' );
+
+$ws_case = new ws_case();
+$ws_case->getCaseDetail();
--- apis/common/ws_case.php
+++ apis/common/ws_case.php
@@ -29,7 +29,7 @@ class ws_case
         $link = $db->getLink();
 
         // sql 查询
-        $query = "select * from " . self::TABLE_NAME . " where type='$type' isDel=0 order by id desc";
+        $query = "select * from " . self::TABLE_NAME . " where type='$type' and isDel=0 order by id desc";
         mysqli_query( $link, "set character set 'utf8'" );//读库
         // 执行sql查询
         $result = mysqli_query( $link, $query ) or die( "sql exec failed" );
@@ -42,6 +42,55 @@ class ws_case
                 "title" => $row['title'],
                 "thumb" => $row['thumb'],
                 "type"  => $row['type' ],
+            );
+        }
+
+        $system->response( $arr );
+
+        $db->free( $result );
+        $db->close( $link );
+    }
+
+    /**
+     * 获取案例详情
+     */
+    public function getCaseDetail () {
+        $id = @$_REQUEST[ 'id' ];
+        $system = new ws_system();
+
+        if ( !$id ) {
+            $system->idNotExist();
+            exit();
+        }
+
+        $db = new ws_db();
+        $link = $db->getLink();
+
+        // sql 查询
+        $query = "select * from " . self::TABLE_NAME . " where isDel=0 and id=" . $id;
+        mysqli_query( $link, "set character set 'utf8'" );//读库
+        // 执行sql查询
+        $result = mysqli_query( $link, $query ) or die( "sql exec failed" );
+
+        if ( !mysqli_num_rows( $result ) ) {
+            $system->rowsNotExist();
+            exit();
+        }
+
+        // 对应案例访问量增加1
+        mysqli_query( $link, "UPDATE " . self::TABLE_NAME . " SET view=view+1 where id=" . $id );
+
+        $arr = [];
+
+        while ( $row = mysqli_fetch_array( $result ) ) {
+            $arr = array(
+                "id"      => $row['id'     ], // 案例id
+                "title"   => $row['title'  ], // 案例标题
+                "type"    => $row['type'   ], // 案例标题
+                "author"  => $row['author' ], // 作者
+                "time"    => $row['time'   ], // 发布时间
+                "content" => $row['content'], // 案例内容
+                "view"    => $row['view'   ], // 浏览次数
             );
         }
 
